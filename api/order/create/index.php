9abbb8e0bc05efa86f0fb856973e169db73f1ea4
José Amietta <joseamietta@gmail.com>
2020-06-26 18:00:47 -0300

    Avanzar con último paso de generación de pago

--- api/order/create/index.php
+++ api/order/create/index.php
@@ -5,17 +5,15 @@
 header('Content-type: application/json');
 
 include_once '../../global/functions.php';
-global $access_token,$collector_id;
+global $access_token, $collector_id;
 
- $external_id = $_POST["external_id"];
- $json = $_POST["json"];
+$external_id = $_POST["external_id"];
+$json = $_POST["json"];
 
- // REVISA AQUÍ:
- // Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder crear una orden
- // en base al external_id, collector_id y Json recibidos
- // Sustituye el método por su correspondiente: get, put, post, delete
- $url = "";
+// REVISA AQUÍ:
+// Qué método y endpoint de la API de Mercado Pago deberías poner aquí para poder crear una orden
+// en base al external_id, collector_id y Json recibidos
+// Sustituye el método por su correspondiente: get, put, post, delete
+$url = "https://api.mercadopago.com/mpmobile/instore/qr/$collector_id/$external_id";
 
-curl_call("MÉTODO","$url?access_token=$access_token",$json);
-
-?>
+curl_call("post", "$url?access_token=$access_token", $json);
